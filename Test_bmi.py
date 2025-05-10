4d808d0e402325a47c6e059fc2db005901c41ff7
Terence <terencetng.24@ichat.sp.edu.sg>
2025-05-10 14:02:11 +0800

    added pytest for bmi

---
+++ Test_bmi.py
@@ -0,0 +1,19 @@
+import Lab2.lab2 as bmi
+
+
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.7, 54)
+
+    assert (result == 0)
+
+
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.7, 73)
+
+    assert (result == 1)
+
+
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.7, 53)
+
+    assert (result == -1)
